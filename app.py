46263727b69281d221d18189b66ba5d74a892966
dave117 (Dave Vedant) <51185825+vedantdave77@users.noreply.github.com>
2020-10-14 11:55:34 -0400

    connect PostgreSQL database to application
    After each 30 minute, heroku shutdowns the endpoint which is generated previously. In such case generated database, and variables deleted itself. For, that we need Postgres Database so, the items will not be deleted and client can use it again.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-'''
-Good Code Practice
-'''
+import os
+
 from flask import Flask
 from flask_restful import Api
 
@@ -12,8 +11,8 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # say where to find the data.db DATAbase
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False # this turn off flask sql alchemy tracker but still sql_alchemy tracker is still in use (running)...
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')   # say where to find the data.db DATAbase, we use both database here one for heroku and other for production
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False                                          # this turn off flask sql alchemy tracker but still sql_alchemy tracker is still in use (running)...
 app.secret_key = 'bob'
 api = Api(app)
 
@@ -21,9 +20,9 @@ jwt = JWT(app, authenticate, identity)
 
 # Going to use seperate item file 
 
-api.add_resource(Item, '/item/<string:name>')    # local_url/studnt/name
-api.add_resource(ItemList, '/items')          # item list
-api.add_resource(Store, '/store/<string:name>')       # Bug fix : no space between string:name other wise url will be malfunctioning.
+api.add_resource(Item, '/item/<string:name>')                                                  # local_url/studnt/name
+api.add_resource(ItemList, '/items')                                                           # item list
+api.add_resource(Store, '/store/<string:name>')                                                # Bug fix : no space between string:name other wise url will be malfunctioning.
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
